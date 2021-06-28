23f432c24568b73cdfe341380b6dda93fcdad8d8
Sai <saisandhya187@gmail.com>
2021-06-28 18:53:49 +0530

    gallery basic layout

--- pages/gallery.js
+++ pages/gallery.js
@@ -1,7 +1,19 @@
 import React from "react";
+import styles from "../styles/Gallery.module.css";
 
 const Gallery = () => {
-  return <div>Galery</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.row}>
+        <div>
+          
+        </div>
+        <div>
+
+        </div>
+      </div>
+    </div>
+  )
 };
 
 export default Gallery;
