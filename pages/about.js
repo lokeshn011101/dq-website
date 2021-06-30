abf805bc90789c8bc557029d1b1bb4a6e0907450
Sai <saisandhya187@gmail.com>
2021-06-30 10:26:38 +0530

    Add AOS to about page

--- pages/about.js
+++ pages/about.js
@@ -1,17 +1,23 @@
-import React from "react";
+import React, {useEffect} from "react";
 import styles from "../styles/About.module.css";
-import Link from 'next/link';
+import QuoteTile from "../components/QuoteTile";
+import AOS from "aos";
 
 const About = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+    });
+  }, []);
   return (
     <div>
       <div className={styles.bg}>
         <div className={styles.container}>
-          <h1 className={styles.title}>Meet Dhiya</h1>
+          <h1 className={styles.title} data-aos="fade-right">Meet Dhiya</h1>
           <div className={styles.person}>
-            <img src="/diya.png" className={styles.image} />
+            <img src="/diya.png" className={styles.image} data-aos="fade-up"/>
             <div className={styles.content}>
-              <p>
+              <p data-aos="fade-up">
                 Each event is carefully designed and coordinated by our founder{" "}
                 <span className={styles.bold}>Dhiya D’Rosario </span>
                 Her strength is to take care of all the little things you hadn’t
@@ -30,12 +36,12 @@ const About = () => {
               </p>
             </div>
           </div>
-          <img src="/flow2.png" className={styles.leaf} />
-          <h1 className={styles.title}>and Roshan</h1>
+          <img src="/flow2.png" className={styles.leaf} data-aos="fade-in"/>
+          <h1 className={styles.title} data-aos="fade-left">and Roshan</h1>
           <div className={styles.person}>
-          <img src="/roshan.png" className={styles.image} />
+          <img src="/roshan.png" className={styles.image} data-aos="fade-down" data-aos-duration="2000"/>
             <div className={styles.content}>
-              <p>
+              <p data-aos="fade-down">
                 The brain behind the business side of things here at DQ,{" "}
                 <span className={styles.bold}>Roshan Kalyan </span> takes care
                 of the logistics behind the whole operation and with her MBA
@@ -51,14 +57,15 @@ const About = () => {
         </div>
       </div>
       <div className={styles.servicecontainer}>
-        <div className={styles.service}>
-          <p>
-            Every event from a birthday to a wedding can be transformed from
-            just a small idea to a magical experience! Take a look at the
-            services we offer and get in touch so we can discuss how to
-            accommodate your requirements!
-          </p>
-          <Link href="/services"><button className={styles.button}>OUR SERVICES</button></Link>
+        <div>
+          <QuoteTile
+            data_aos_text="fade-down"
+            data_aos_button="fade-up"
+            data_aos_button-delay="250"
+            text="Every event from a birthday to a wedding can be transformed from just a small idea to a magical experience! Take a look at the
+services we offer and get in touch so we can discuss how to accommodate your requirements!"
+            buttext="OUR SERVICES"
+          />
         </div>
       </div>
     </div>
