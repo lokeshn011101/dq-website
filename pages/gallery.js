83a7ce361ecef10f9499694ec4aa073418be27f1
Sai <saisandhya187@gmail.com>
2021-07-08 19:11:15 +0530

    add gallery photos

--- pages/gallery.js
+++ pages/gallery.js
@@ -4,12 +4,33 @@ import styles from "../styles/Gallery.module.css";
 const Gallery = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.row}>
+      <div className={styles.row1}>
         <div>
-          
+          <img src='/gallery-1.png' />
         </div>
         <div>
-
+          <img src='/gallery-2.png' />
+        </div>
+         <div>
+          <img src='/gallery-3.png' />
+        </div>
+        <div>
+          <img src='/gallery-4.png' />
+        </div>
+         <div>
+          <img src='/gallery-5.png' />
+        </div>
+        <div>
+          <img src='/gallery-6.png' />
+        </div>
+        <div>
+          <img src='/gallery-7.png' />
+        </div>
+        <div>
+          <img src='/gallery-8.png' />
+        </div>
+        <div>
+          <img src='/gallery-9.png' />
         </div>
       </div>
     </div>
