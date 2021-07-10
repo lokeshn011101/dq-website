0117ccadde374774016b4a4b63540d05ee3ed7fb
Sai <saisandhya187@gmail.com>
2021-07-10 19:11:42 +0530

    gallery page responsiveness

--- pages/gallery.js
+++ pages/gallery.js
@@ -11,26 +11,34 @@ const Gallery = () => {
         <div>
           <img src='/gallery-2.png' />
         </div>
-         <div>
+      </div>
+      <div className={styles.even}>
+        <div>
           <img src='/gallery-3.png' />
         </div>
         <div>
           <img src='/gallery-4.png' />
         </div>
-         <div>
+      </div>
+      <div className={styles.one}>
+        <div>
           <img src='/gallery-5.png' />
         </div>
+      </div>
+      <div className={styles.row2}>
         <div>
           <img src='/gallery-6.png' />
         </div>
         <div>
           <img src='/gallery-7.png' />
         </div>
+      </div>
+      <div className={styles.even}>
         <div>
-          <img src='/gallery-8.png' />
+           <img src='/gallery-8.png' />
         </div>
         <div>
-          <img src='/gallery-9.png' />
+           <img src='/gallery-9.png' />
         </div>
       </div>
     </div>
