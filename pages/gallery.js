58f23fdbbaf961939237b6796ae0ad9e37335b2f
lokeshn011101 <lokesh19055@cse.ssn.edu.in>
2021-07-12 14:40:17 +0530

    added code for gallery

--- pages/gallery.js
+++ pages/gallery.js
@@ -4,44 +4,43 @@ import styles from "../styles/Gallery.module.css";
 const Gallery = () => {
   return (
     <div className={styles.container}>
-      {/* <div className={styles.row1}>
+      <div className={styles.row1}>
         <div>
-          <img src='/gallery-1.png' />
+          <img src="/gallery-1.png" />
         </div>
         <div>
-          <img src='/gallery-2.png' />
+          <img src="/gallery-2.png" />
         </div>
       </div>
       <div className={styles.even}>
         <div>
-          <img src='/gallery-3.png' />
+          <img src="/gallery-3.png" />
         </div>
         <div>
-          <img src='/gallery-4.png' />
+          <img src="/gallery-4.png" />
         </div>
       </div>
       <div className={styles.one}>
         <div>
-          <img src='/gallery-5.png' />
+          <img src="/gallery-5.png" />
         </div>
       </div>
       <div className={styles.row2}>
         <div>
-          <img src='/gallery-6.png' />
+          <img src="/gallery-6.png" />
         </div>
         <div>
-          <img src='/gallery-7.png' />
+          <img src="/gallery-7.png" />
         </div>
       </div>
       <div className={styles.even}>
         <div>
-           <img src='/gallery-8.png' />
+          <img src="/gallery-8.png" />
         </div>
         <div>
-           <img src='/gallery-9.png' />
+          <img src="/gallery-9.png" />
         </div>
-      </div> */}
-      gallery
+      </div>
     </div>
   );
 };
