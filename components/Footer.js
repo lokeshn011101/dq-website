88a53026beaa99f09960bef80005632e5c227b8f
lokeshn011101 <lokesh19055@cse.ssn.edu.in>
2021-07-15 12:18:11 +0530

    deleted commits and made changes

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Link from "next/link";
 import { useRouter } from "next/router";
 
 const Footer = () => {
@@ -8,7 +7,7 @@ const Footer = () => {
     <div className="footer mx-auto px-16 mdd:px-4 lg:mt-40 md:mt-36 sm:mt-24 smm:mt-20 bg-footer-pink flex smm:flex-col items-center justify-around lg:text-lg md:text-base sm:text-sm smm:text-sm">
       <div className="leftft lg:pl-16 flex flex-col justify-center items-start smm:items-center w-1/6 sm:w-2/6 smm:w-full text-white font-light">
         <div>The Design Quotient</div>
-        <div> +91 9840484457</div>
+        <div>+91 9840484457</div>
         <div>info@designquotient.net</div>
       </div>
       <div className="centerft uppercase text-5xl mdd:text-3xl text-white w-3/6 sm:w-2/6 smm:w-full text-center font-ostrich tracking-widest">
