089f07f0faf7fa3bf85da65ea15d51ec6068b467
mofos <isaacjohn2310@gmail.com>
2021-07-21 09:28:57 +0530

    gallery fix

--- pages/gallery.js
+++ pages/gallery.js
@@ -9,42 +9,43 @@ const Gallery = () => {
     });
   }, []);
   return (
+    
     <div className={styles.container}>
       <div className={styles.row1}>
         <div>
-          <img src="/gallery-1.png" data-aos="fade-left" />
+          <img src="/gallery-1.png" data-aos="fade-left" className={styles.gal2}/>
         </div>
-        <div>
-          <img src="/gallery-2.png" data-aos="fade-left" />
+        <div >
+        <img src="/gallery-2.png" data-aos="fade-left" className={styles.gal2}/>
         </div>
       </div>
       <div className={styles.even}>
         <div>
-          <img src="/gallery-3.png" data-aos="fade-right" />
+          <img src="/gallery-3.png" data-aos="fade-right" className={styles.gal2} />
         </div>
         <div>
-          <img src="/gallery-4.png" data-aos="fade-right" />
+          <img src="/gallery-4.png" data-aos="fade-right" className={styles.gal2} />
         </div>
       </div>
       <div className={styles.one}>
         <div>
-          <img src="/gallery-5.png" data-aos="fade-left" />
+          <img src="/gallery-5.png" data-aos="fade-left" className={styles.gal2}/>
         </div>
       </div>
       <div className={styles.row2}>
         <div>
-          <img src="/gallery-6.png" data-aos="fade-right" />
+          <img src="/gallery-6.png" data-aos="fade-right" className={styles.gal2}/>
         </div>
         <div>
-          <img src="/gallery-7.png" data-aos="fade-right" />
+          <img src="/gallery-7.png" data-aos="fade-right" className={styles.gal2}/>
         </div>
       </div>
       <div className={styles.even}>
         <div>
-          <img src="/gallery-8.png" data-aos="fade-left" />
+          <img src="/gallery-8.png" data-aos="fade-left" className={styles.gal2}/>
         </div>
         <div>
-          <img src="/gallery-9.png" data-aos="fade-left" />
+          <img src="/gallery-9.png" data-aos="fade-left" className={styles.gal2} />
         </div>
       </div>
     </div>
