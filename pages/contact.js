cc3d0c9a89699b7c66b8e75faea0c502ce0ea3c8
lokeshn011101 <lokesh19055@cse.ssn.edu.in>
2021-07-25 22:40:58 +0530

    contact overflow hidden

--- pages/contact.js
+++ pages/contact.js
@@ -58,76 +58,78 @@ const Contact = () => {
   };
 
   return (
-    <div
-      className="contact w-full flex lg:justify-end justify-center items-center"
-      data-aos="fade-left"
-    >
-      <form
-        type="submit"
-        className="cn-form h-full lg:w-1/2 md:w-9/12 sm:w-10/12 smm:w-11/12 flex flex-col justify-around items-start lg:pl-10"
+    <div className="overflow-hidden">
+      <div
+        className="contact w-full flex lg:justify-end justify-center items-center"
         data-aos="fade-left"
-        data-aos-delay="300"
       >
-        <div className="con-tt sm:text-5xl smm:text-4xl lgg:mx-auto lgg:my-4">
-          <div>GET IN TOUCH!</div>
-        </div>
-        <div className="w-full name-contact flex vsmm:flex-col vsmm:justify-center vsmm:items-center justify-start items-start">
-          <div className="cname w-1/2 vsmm:w-10/12 lg:pr-8 vsm:px-5 vsmm:flex vsmm:flex-col vsmm:justify-center vsmm:items-center vsmm:pb-5">
-            <div className="ciname italic text-xl">Name</div>
-            <input
-              onChange={(txt) => setName(txt)}
-              type="text"
-              className="ciiname w-full h-10 bg-none border-2 border-black text-lg"
-            />
+        <form
+          type="submit"
+          className="cn-form h-full lg:w-1/2 md:w-9/12 sm:w-10/12 smm:w-11/12 flex flex-col justify-around items-start lg:pl-10"
+          data-aos="fade-left"
+          data-aos-delay="300"
+        >
+          <div className="con-tt sm:text-5xl smm:text-4xl lgg:mx-auto lgg:my-4">
+            <div>GET IN TOUCH!</div>
           </div>
-          <div className="ccontact w-1/2 vsmm:w-10/12 lg:pr-8 vsm:px-5 vsmm:flex vsmm:flex-col vsmm:justify-center vsmm:items-center vsmm:pb-5">
-            <div className="cicontact italic text-xl">Contact</div>
-            <input
-              onChange={(txt) => setContact(txt)}
-              type="text"
-              className="ciicontact w-full h-10 bg-none border-2 border-black text-lg"
-            />
+          <div className="w-full name-contact flex vsmm:flex-col vsmm:justify-center vsmm:items-center justify-start items-start">
+            <div className="cname w-1/2 vsmm:w-10/12 lg:pr-8 vsm:px-5 vsmm:flex vsmm:flex-col vsmm:justify-center vsmm:items-center vsmm:pb-5">
+              <div className="ciname italic text-xl">Name</div>
+              <input
+                onChange={(txt) => setName(txt)}
+                type="text"
+                className="ciiname w-full h-10 bg-none border-2 border-black text-lg"
+              />
+            </div>
+            <div className="ccontact w-1/2 vsmm:w-10/12 lg:pr-8 vsm:px-5 vsmm:flex vsmm:flex-col vsmm:justify-center vsmm:items-center vsmm:pb-5">
+              <div className="cicontact italic text-xl">Contact</div>
+              <input
+                onChange={(txt) => setContact(txt)}
+                type="text"
+                className="ciicontact w-full h-10 bg-none border-2 border-black text-lg"
+              />
+            </div>
           </div>
-        </div>
-        <div className="w-full email-service flex vsmm:flex-col vsmm:justify-center vsmm:items-center justify-start items-start">
-          <div className="cemail  w-1/2 vsmm:w-10/12 lg:pr-8 vsm:px-5 vsmm:flex vsmm:flex-col vsmm:justify-center vsmm:items-center vsmm:pb-5">
-            <div className="ciemail italic text-xl">Email*</div>
-            <input
-              onChange={(txt) => setEmail(txt)}
-              type="text"
-              className="ciiemail w-full h-10 bg-none border-2 border-black text-lg"
-            />
-          </div>
-          <div className="cservice  w-1/2 vsmm:w-10/12 lg:pr-8 vsm:px-5 vsmm:flex vsmm:flex-col vsmm:justify-center vsmm:items-center vsmm:pb-5">
-            <div className="ciservice italic text-xl">Select Service*</div>
-            {/* <input
+          <div className="w-full email-service flex vsmm:flex-col vsmm:justify-center vsmm:items-center justify-start items-start">
+            <div className="cemail  w-1/2 vsmm:w-10/12 lg:pr-8 vsm:px-5 vsmm:flex vsmm:flex-col vsmm:justify-center vsmm:items-center vsmm:pb-5">
+              <div className="ciemail italic text-xl">Email*</div>
+              <input
+                onChange={(txt) => setEmail(txt)}
+                type="text"
+                className="ciiemail w-full h-10 bg-none border-2 border-black text-lg"
+              />
+            </div>
+            <div className="cservice  w-1/2 vsmm:w-10/12 lg:pr-8 vsm:px-5 vsmm:flex vsmm:flex-col vsmm:justify-center vsmm:items-center vsmm:pb-5">
+              <div className="ciservice italic text-xl">Select Service*</div>
+              {/* <input
               onChange={(txt) => setService(txt)}
               type="text"
               className="ciiservice w-full h-10 bg-none border-2 border-black text-lg"
             /> */}
-            <Dropdown setServices={setServices} />
+              <Dropdown setServices={setServices} />
+            </div>
           </div>
-        </div>
-        <div className="w-full message vsm:px-5 vsmm:flex vsmm:flex-col vsmm:justify-center vsmm:items-center ">
-          <div className="cmsg text-xl">Message</div>
-          <textarea
-            onChange={(txt) => setMessage(txt)}
-            type="text"
-            className="cimsg lg:w-1/2 border-2 border-black text-lg"
-            rows="8"
-            cols={winWidth > 425 ? "30" : "28"}
-          ></textarea>
-        </div>
-        <div className="flex flex-row w-full vsmm:justify-center vsmm:items-center vsmm:mx-auto px-5 vsmm:py-5 vsmm:pt-8">
-          <button
-            onClick={onFormSubmit}
-            className="submit-button px-10 py-4 mb-4 submit text-2xl"
-            type="submit"
-          >
-            SUBMIT
-          </button>
-        </div>
-      </form>
+          <div className="w-full message vsm:px-5 vsmm:flex vsmm:flex-col vsmm:justify-center vsmm:items-center ">
+            <div className="cmsg text-xl">Message</div>
+            <textarea
+              onChange={(txt) => setMessage(txt)}
+              type="text"
+              className="cimsg lg:w-1/2 border-2 border-black text-lg"
+              rows="8"
+              cols={winWidth > 425 ? "30" : "28"}
+            ></textarea>
+          </div>
+          <div className="flex flex-row w-full vsmm:justify-center vsmm:items-center vsmm:mx-auto px-5 vsmm:py-5 vsmm:pt-8">
+            <button
+              onClick={onFormSubmit}
+              className="submit-button px-10 py-4 mb-4 submit text-2xl"
+              type="submit"
+            >
+              SUBMIT
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
